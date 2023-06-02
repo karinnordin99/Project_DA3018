5b403aa7c2c0ab7d6fcde19292a64b039574bbdc
supergurkan <133381081+supergurkan@users.noreply.github.com>
2023-06-02 18:20:35 +0200

    Update translate_data.sh

--- translate_data.sh
+++ translate_data.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Author : Karin Nordin
+echo "This project has been applies to a smaller dataset, beginning by extracting a sampla from the given data with 1.000.000 lines"
+head -n 1000000 /Users/karin/DA3018/Projekt/Project_DA3018/Spruce_fingerprint_2017-03-10_16.48.olp.m4 > samplemillion.txt
 
 awk '
 BEGIN {
@@ -18,9 +20,14 @@ $7 - $6 + 1 >= 1000 && $11 - $10 + 1 >= 1000 {
     print vertex_map[$1], vertex_map[$2], $6, $7, $8, $10, $11, $12
 }
 ' samplemillion.txt > "$translated_data_1_000_000.txt"
+echo "The sample has been translated, strings has been mapped to integers and only the contigs fulfilling the criterea n>=1000 are included"
+echo "Now running the java project"
+java projekt.java > translated_data_1_000_000.txt
 
-java projekt.java > million.txt
+touch component_density_million.txt
+touch node_density_million.txt
 
-# Dividing the output data into component_density_million.txt (first array) and node_density_million.txt
+head -4 component_density_million.txt
+head -5 node_density_million.txt
 
 Python3 histo_plot.py 
