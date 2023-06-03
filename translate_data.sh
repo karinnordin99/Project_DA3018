a8e2b3a9e6d02625db7d9c612eeec1a19c64a44c
supergurkan <133381081+supergurkan@users.noreply.github.com>
2023-06-03 19:37:06 +0200

    Update translate_data.sh

--- translate_data.sh
+++ translate_data.sh
@@ -10,7 +10,6 @@
 echo "This project has been applies to a smaller dataset, beginning by extracting a sampla from the given data with 1.000.000 lines"
 
 touch samplemillion.txt
-touch translated_data_1_000_000.txt
 
 head -n 1000000 /Users/karin/DA3018/Projekt/Run/Spruce_fingerprint_2017-03-10_16.48.olp.m4 > samplemillion.txt
 
