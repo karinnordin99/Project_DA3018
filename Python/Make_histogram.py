c8ee158cece0968916b5c1dfe7dd91deed6f5b78
supergurkan <133381081+supergurkan@users.noreply.github.com>
2023-06-03 16:41:48 +0200

    Update Make_histogram.py

--- Python/Make_histogram.py
+++ Python/Make_histogram.py
@@ -9,41 +9,9 @@ Created on Thu Jun  1 17:17:40 2023
 import matplotlib.pyplot as plt
 import math
 
-#node_degrees_data = "/Users/karin/DA3018/Projekt/Project_DA3018/node_densities_million.txt"
-#component_densities_data = "/Users/karin/DA3018/Projekt/Project_DA3018/Componenent_densities.txt"
-node_degrees_data_5000 = "/Users/karin/eclipse-workspace/projekt/src/projekt/node_degree_5000.txt"
-component_densities_data_5000 = "/Users/karin/eclipse-workspace/projekt/src/projekt/component_density_5000.txt"
-node_degrees_data_million = "/Users/karin/eclipse-workspace/projekt/src/projekt/node_density_million.txt"
-component_densities_data_million = "/Users/karin/eclipse-workspace/projekt/src/projekt/component_density_million.txt"
+node_degrees_data_million = "/Users/karin/DA3018/Projekt/Project_DA3018/testrunning/node_density_million.txt"
+component_densities_data_million = "/Users/karin/DA3018/Projekt/Project_DA3018/testrunning/component_density_million.txt"
 def plot_component_densities():
-    
-    with open(node_degrees_data_million, 'r') as file:
-        
-        array_component = file.read()
-        clean_array_component = array_component.replace("{", "").replace("}","").replace(",", "").replace("=", " ")
-        cleaner_array_component = clean_array_component.split(" ")
-        array_list = [int(x) for x in cleaner_array_component]
-        
-        keys = []
-        values = []
-        
-        end_index = math.floor(len(array_list)/2)
-        
-        for i in range(end_index):
-            
-            keys.append(array_list[2*i])
-            values.append(array_list[2*i +1])
-            
-        plt.bar(keys, values)
-        plt.xlabel('degrees')
-        plt.ylabel('Values')
-        plt.yscale("log")
-        plt.title('Component Densities Histogram')
-        plt.show()
-    
-plot_component_densities()
-
-def plot_node_degree():
     
     with open(component_densities_data_million, 'r') as file:
         
@@ -62,9 +30,39 @@ def plot_node_degree():
             keys.append(array_list[2*i])
             values.append(array_list[2*i +1])
             
+        plt.bar(keys, values)
+        plt.xlabel('Density')
+        plt.ylabel('Component')
+        plt.yscale("log")
+        plt.title('Component Densities Histogram')
+        plt.show()
+    
+plot_component_densities()
+
+def plot_node_degree():
+    
+    with open(node_degrees_data_million, 'r') as file:
+        
+        array_component = file.read()
+        clean_array_component = array_component.replace("{", "").replace("}","").replace(",", "").replace("=", " ")
+        cleaner_array_component = clean_array_component.split(" ")
+        array_list = [float(x) for x in cleaner_array_component]
+        
+        keys = []
+        values = []
+
+        end_index = math.floor(len(array_list)/2)
+        
+        for i in range(end_index):
+            
+            keys.append(array_list[2*i])
+            values.append(array_list[2*i +1])
+        print(keys)
+        print(values)
+            
         plt.bar(keys, values, color="m")
-        plt.xlabel('Keys')
-        plt.ylabel('Values')
+        plt.xlabel('Degree')
+        plt.ylabel('Component')
         plt.yscale("log")
         plt.title('Node degree Histogram')
         plt.show()
